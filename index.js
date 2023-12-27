0c533933da3c48e3d533a8b5b113deb04c3d64a8
Le Vivilet <levivlet@pm.me>
2023-12-27 22:27:13 +0100

    try to create pull request on release

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const getBranchHeadSha = async (ctx, owner, repo) => {
+  const res = await ctx.octokit.git.getRef({
+    owner,
+    repo,
+    ref: `heads/main`,
+  });
+  const ref = res.data.object;
+  return ref.sha;
+};
+
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
@@ -13,15 +23,30 @@ module.exports = (app) => {
     return context.octokit.issues.createComment(issueComment);
   });
 
-  app.on("release.released", (info) => {
+  app.on("release.released", async (info) => {
     const tagName = info.payload.release.tag_name;
+    const owner = info.payload.repository.owner.login;
+    const repo = "test-repo-a";
     console.log(tagName);
     console.log("release was released" + info.payload.repository.name);
+
+    const branchName = `update-version/${tagName}`;
+
+    const sha = await getBranchHeadSha(owner, repo);
+    const result = await info.octokit.git.createRef({
+      owner,
+      repo,
+      ref: `refs/heads/${branchName}`,
+      sha,
+    });
+    const branchSha = result.data.object.sha;
+    const { data: pr } = await info.octokit.pulls.create({
+      owner,
+      repo,
+      head: branchName,
+      base: "main",
+      body: `update to version ${tagName}`,
+    });
+    console.log("created pr");
   });
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
