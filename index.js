866b2e5b6819caaa764bcffb05a402745f17be2e
Le Vivilet <levivlet@pm.me>
2023-12-27 23:16:36 +0100

    try to fix tree error

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const { Context } = require("probot");
+
+/**
+ *
+ * @param {Context} ctx
+ * @param {*} owner
+ * @param {*} repo
+ * @param {*} branch
+ * @returns
+ */
 const getBranchHeadSha = async (ctx, owner, repo, branch) => {
   const res = await ctx.octokit.rest.git.getRef({
     owner,
@@ -24,40 +34,56 @@ module.exports = (app) => {
   });
 
   app.on("release.released", async (context) => {
-    const tagName = context.payload.release.tag_name;
-    const owner = context.payload.repository.owner.login;
+    const { payload, octokit } = context;
+    const tagName = payload.release.tag_name;
+    const owner = payload.repository.owner.login;
+    const baseBranch = "main";
     const repo = "test-repo-a";
     console.log(tagName);
-    console.log("release was released" + context.payload.repository.name);
+    console.log("release was released" + payload.repository.name);
 
-    console.log("git is", typeof context.octokit.rest.git);
-    const branchName = `update-version/${tagName}`;
+    const newBranch = `update-version/${tagName}`;
 
-    const mainBranchSha = await getBranchHeadSha(context, owner, repo, "main");
-    const result = await context.octokit.rest.git.createRef({
+    const mainBranchRef = await octokit.rest.git.getRef({
       owner,
       repo,
-      ref: `refs/heads/${branchName}`,
-      sha: mainBranchSha,
+      ref: `heads/${baseBranch}`,
+    });
+    const newBranchRef = await octokit.rest.git.createRef({
+      owner,
+      repo,
+      ref: `refs/heads/${newBranch}`,
+      sha: mainBranchRef.data.object.sha,
     });
     console.log("created branch");
 
-    // const branchSha = result.data.object.sha;
-    const branchSha = await getBranchHeadSha(context, owner, repo, branchName);
-
-    console.log("branch sha", branchSha);
-    await context.octokit.rest.git.createCommit({
-      message: "test commit",
+    const currentCommit = await octokit.rest.git.getCommit({
       owner,
       repo,
-      tree: branchSha,
+      commit_sha: newBranchRef.data.object.sha,
     });
-    // const { data: pr } = await context.octokit.pulls.create({
-    //   owner,
-    //   repo,
-    //   head: branchName,
-    //   base: "main",
-    //   body: `update to version ${tagName}`,
-    // });
+
+    const newCommit = await octokit.rest.git.createCommit({
+      owner,
+      repo,
+      message: "test commit",
+      tree: currentCommit.data.tree.sha,
+      parents: [currentCommit.data.sha],
+    });
+
+    await octokit.rest.git.updateRef({
+      owner,
+      repo,
+      ref: `heads/${newBranch}`,
+      sha: newCommit.data.sha,
+    });
+
+    await octokit.rest.pulls.create({
+      owner,
+      repo,
+      head: newBranch,
+      base: baseBranch,
+      title: `update to version ${tagName}`,
+    });
   });
 };
